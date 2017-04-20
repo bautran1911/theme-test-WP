ff906feae1bddec0fa776bed5166b88a7bd95a4a
Bau Tran <bautran1911@gmail.com>
2017-04-20 22:09:19 +0700

    Format code function

--- functions.php
+++ functions.php
@@ -1,62 +1,62 @@
 <?php 
-add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
-add_theme_support( 'menus' );
+	add_theme_support( 'menus' );
 
-register_nav_menus(
-        array(
-		'main-nav-sm' => 'Menu Mobile',
-                'main-nav' => 'Menu chính',
-                'footer-nav' => 'Footer menu'
-        )
-);
+	register_nav_menus(
+		array(
+			'main-nav-sm' => 'Menu Mobile',
+			'main-nav' => 'Menu chính',
+			'footer-nav' => 'Footer menu'
+		)
+	);
 
 
 
 	add_filter( 'menu_image_default_sizes', function($sizes){
 
-     // remove the default 36x36 size
+	// remove the default 36x36 size
 
-     unset($sizes['menu-36x36']);
+	unset($sizes['menu-36x36']);
 
-     // add a new size
+	// add a new size
 
-     $sizes['menu-100x100'] = array(100,100);
+	$sizes['menu-100x100'] = array(100,100);
 
-     // return $sizes (required)
+	// return $sizes (required)
 
-     return $sizes;
+	return $sizes;
 
-});
+	});
 
 
-function footer_customize_register($wp_customize) 
-{
-	$wp_customize->add_section("ads", array(
-		"title" => __("Footer", "customizer_ads_sections"),
-		"priority" => 30,
-	));
+	function footer_customize_register($wp_customize) 
+	{
+		$wp_customize->add_section("ads", array(
+			"title" => __("Footer", "customizer_ads_sections"),
+			"priority" => 30,
+		));
 
-        $wp_customize->add_setting("footer_text", array(
-		"default" => "",
-		"transport" => "postMessage",
-	));
+		$wp_customize->add_setting("footer_text", array(
+			"default" => "",
+			"transport" => "postMessage",
+		));
 
-        $wp_customize->add_control(new WP_Customize_Control(
-		$wp_customize,
-		"ads_text",
-		array(
-			"label" => __("Enter Footer Text", "customizer_ads_text_label"),
-			"section" => "ads",
-			"settings" => "footer_text",
-			"type" => "textarea",
-		)
-	));
+		$wp_customize->add_control(new WP_Customize_Control(
+			$wp_customize,
+			"ads_text",
+			array(
+				"label" => __("Enter Footer Text", "customizer_ads_text_label"),
+				"section" => "ads",
+				"settings" => "footer_text",
+				"type" => "textarea",
+			)
+		));
 
-        wp_enqueue_script("footer-themecustomizer", get_template_directory_uri() . "/theme-customizer.js", array("jquery", "customize-preview"), '',  true);
-}
+		wp_enqueue_script("footer-themecustomizer", get_template_directory_uri() . "/theme-customizer.js", array("jquery", "customize-preview"), '',  true);
+	}
 
-add_action("customize_register","footer_customize_register");
+	add_action("customize_register","footer_customize_register");
 
 
 
